deb92018ca9073517506116b65110a3e1bc83989
Agustin <agustincampos04@gmail.com>
2025-05-14 09:31:54 -0300

    cree test y modelo: Universidad. Close #2

---
+++ app/models/universidad.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+from app.models.facultad import Facultad
+
+@dataclass(init=False, repr=True, eq=True)
+class Universidad(Facultad):
+    nombre: str
+    sigla: str
