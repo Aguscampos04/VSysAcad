0b8fff345d93098158797be81cb04c9c3c691352
Agustin <agustincampos04@gmail.com>
2025-06-06 09:48:36 -0300

    saque los unique de los modelos

--- app/models/materia.py
+++ app/models/materia.py
@@ -6,5 +6,5 @@ class Materia(db.Model):
     __tablename__ = "materias"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(255), nullable=False)
-    codigo = db.Column(db.String(20), nullable=False, unique=True)
+    codigo = db.Column(db.String(20), nullable=False)
     observacion = db.Column(db.String(255), nullable=True)
