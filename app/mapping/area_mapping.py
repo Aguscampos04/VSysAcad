e76a6a33553e7ede43460c970174cd54ee2cce44
Agustin <agustincampos04@gmail.com>
2025-06-07 22:14:44 -0300

    hice el resource y mapping de area. Close #96

---
+++ app/mapping/area_mapping.py
@@ -0,0 +1,10 @@
+from marshmallow import fields, Schema, post_load, validate
+from app.models import Area
+
+class AreaMapping(Schema):
+    id = fields.Integer()
+    nombre = fields.String(required=True, validate=validate.Length(min=1, max=100))
+    
+    @post_load
+    def nueva_area(self, data, **kwargs):
+        return Area(**data)
