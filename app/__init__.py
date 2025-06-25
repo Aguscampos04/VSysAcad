7706d3cdb70fbe24a77ffeaf72b535e42a8dd2ae
leacanton04 <leandro2canton@gmail.com>
2025-06-25 15:40:54 -0300

    resource y mapping de plan. Close  #103

--- app/__init__.py
+++ app/__init__.py
@@ -25,7 +25,7 @@ def create_app() -> Flask:
     migrate.init_app(app, db)
     ma.init_app(app)
 
-    from app.resources import home, universidad_bp, area_bp, tipodocumento_bp, tipodedicacion_bp, categoriacargo_bp, grupo_bp, grado_bp, departamento_bp, certificado_bp, tipo_especialidad_bp
+    from app.resources import home, universidad_bp, area_bp, tipodocumento_bp, tipodedicacion_bp, categoriacargo_bp, grupo_bp, grado_bp, departamento_bp, certificado_bp, tipo_especialidad_bp, plan_bp
     app.register_blueprint(home, url_prefix='/api/v1')
     app.register_blueprint(universidad_bp, url_prefix='/api/v1')
     app.register_blueprint(area_bp, url_prefix='/api/v1')
@@ -37,6 +37,7 @@ def create_app() -> Flask:
     app.register_blueprint(departamento_bp, url_prefix='/api/v1')
     app.register_blueprint(certificado_bp, url_prefix='/api/v1')
     app.register_blueprint(tipo_especialidad_bp, url_prefix='/api/v1/')   
+    app.register_blueprint(plan_bp, url_prefix='/api/v1/')   
 
     @app.shell_context_processor
     def ctx():
