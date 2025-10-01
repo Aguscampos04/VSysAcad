61d9d4c2951f7e3f6b3f428e3da804f41eab422a
Loana <anayloanavargad@gmail.com>
2025-10-01 16:56:45 -0300

    hice hashid de grupo. Close #128

--- app/mapping/grupo_mapping.py
+++ app/mapping/grupo_mapping.py
@@ -3,9 +3,8 @@ from app.models import Grupo
 
 
 class GrupoMapping(Schema):
-    id = fields.Integer(dump_only=True)
-    nombre = fields.String(
-        required=True, validate=validate.Length(min=1, max=50))
+    hashid = fields.String(dump_only = True)
+    nombre = fields.String(required=True, validate=validate.Length(min=1, max=50))
 
     @post_load
     def nuevo_grupo(self, data, **kwargs):
