c307f99d9a5e6f3693549917c7ea8d7daafca704
Delfina Carribero <delficarribero@gmail.com>
2025-10-01 16:21:09 -0300

    Hice el hashid de categoria cargo. Close #125

--- app/mapping/categoriacargo_mapping.py
+++ app/mapping/categoriacargo_mapping.py
@@ -2,6 +2,7 @@ from marshmallow import fields, Schema, post_load, validate
 from app.models import CategoriaCargo
 
 class CategoriaCargoMapping(Schema):
+    hashid = fields.String(dump_only = True)
     id = fields.Integer(dump_only=True)
     nombre = fields.String(required=True, validate=validate.Length(min=1, max=30))
     
